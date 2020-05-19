94aa2cf57d3da2ff982357896a3b9fa581bb7cd6
Tobias Dittmann <hello@ditsche.dev>
2020-05-19 17:45:11 +0200

    Added UrlRule to check for valid urls based on ESAPI pattern

---
+++ src/main/java/dev/ditsche/validator/rule/ruleset/UrlRule.java
@@ -0,0 +1,32 @@
+package dev.ditsche.validator.rule.ruleset;
+
+import dev.ditsche.validator.rule.Rule;
+import dev.ditsche.validator.rule.RuleResult;
+
+import java.util.regex.Pattern;
+
+/**
+ * Checks whether a given String is a valid url based on the validation pattern provided by ESAPI.
+ *
+ * More information: https://raw.githubusercontent.com/ESAPI/esapi-java-legacy/develop/configuration/esapi/validation.properties
+ *
+ * @author Tobias Dittmann
+ */
+public class UrlRule implements Rule {
+
+    private final String PATTERN = "^(ht|f)tp(s?)://[0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*(:(0-9)*)*(/?)([a-zA-Z0-9\\-.?,:'/\\\\+=&;%$#_]*)?$";
+
+    @Override
+    public RuleResult passes(Object value) {
+
+        if(!(value instanceof String))
+            RuleResult.reject();
+
+        return RuleResult.passes(Pattern.matches(PATTERN, String.valueOf(value)));
+    }
+
+    @Override
+    public String message(String field) {
+        return null;
+    }
+}
