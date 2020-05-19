f818d7e2ebf9360882ea657af8c5eb71bd4d837b
Tobias Dittmann <hello@ditsche.dev>
2020-05-19 17:48:44 +0200

    Replaced javax mail with pattern for email validation

--- src/main/java/dev/ditsche/validator/rule/ruleset/EmailRule.java
+++ src/main/java/dev/ditsche/validator/rule/ruleset/EmailRule.java
@@ -3,15 +3,18 @@ package dev.ditsche.validator.rule.ruleset;
 import dev.ditsche.validator.rule.Rule;
 import dev.ditsche.validator.rule.RuleResult;
 
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
+import java.util.regex.Pattern;
 
 /**
  * Defines a validation rule.
  * Checks if the given fields value is a valid email address
- * defined by RFC 822.
+ * defined by the pattern of ESAPI.
+ *
+ * @author Tobias Dittmann
  */
 public class EmailRule implements Rule {
+
+    private final String PATTERN = "^[A-Za-z0-9._%'-]+@[A-Za-z0-9.-]+\\\\.[a-zA-Z]{2,4}$";
 
     @Override
     public RuleResult passes(Object value) {
@@ -21,13 +24,7 @@ public class EmailRule implements Rule {
         if(!(value instanceof String))
             return RuleResult.reject();
 
-        try {
-            InternetAddress address = new InternetAddress((String) value);
-            address.validate();
-            return RuleResult.resolve();
-        } catch (AddressException e) {
-            return RuleResult.reject();
-        }
+        return RuleResult.passes(Pattern.matches(PATTERN, String.valueOf(value)));
     }
 
     @Override
