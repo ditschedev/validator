be922a3ecd07a463264f29544082bee20ad726a2
Tobias Dittmann <hello@ditsche.dev>
2020-06-24 23:43:25 +0200

    Created Rule for checking booleans

---
+++ src/main/java/dev/ditsche/validator/rule/ruleset/BooleanRule.java
@@ -0,0 +1,26 @@
+package dev.ditsche.validator.rule.ruleset;
+
+import dev.ditsche.validator.rule.Rule;
+import dev.ditsche.validator.rule.RuleResult;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author Tobias Dittmann
+ */
+@AllArgsConstructor
+public class BooleanRule implements Rule {
+
+    private final boolean val;
+
+    @Override
+    public RuleResult passes(Object value) {
+        if(!(value instanceof Boolean))
+            return RuleResult.reject();
+        return RuleResult.passes((boolean) value == val);
+    }
+
+    @Override
+    public String message(String field) {
+        return String.format("The field \"%s\" needs to be %s", field, val ? "truthy" : "falsy");
+    }
+}
